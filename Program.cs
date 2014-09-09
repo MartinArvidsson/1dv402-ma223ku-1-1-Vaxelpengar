3c6820383ca6222ea2d0fc36b61e36cb9b048bf1
Martin Arvidsson UD14 <ma223ku@student.lnu.se>
2014-09-09 20:26:50 +0200

    Syncar efter att ha påbörjat att på in en avrundningsmetod i programmet

--- Program.cs
+++ Program.cs
@@ -12,26 +12,40 @@ namespace Växelpengar_labration_Martin_Arvidsson_UD14
         {
             // Anger Variabler 
             double TotalSum;
-            double MoneyFromCustomer;
-            double FinalPrice;
+            int MoneyFromCustomer;
+            uint FinalPrice;
+            double RoundingOff;
+            
 
             //Frågar om hur mkt kunden ska betala, gör också om strängen till ett nummer som programmet kan jobba med
+
             Console.Write("Ange den totala summan pengar som ska betalas: ");
             TotalSum = double.Parse(Console.ReadLine());
 
             //Frågar om hur mkt pengar kunden gav dig och läser om strängen till ett nummer som programmet kan jobba med
+
             Console.Write("Ange pengarna som kunden har gett dig: ");
-            MoneyFromCustomer = double.Parse(Console.ReadLine());
+            MoneyFromCustomer = int.Parse(Console.ReadLine());
+
             //Presenterar Grafiskt
 
             Console.WriteLine("KVITTO");
             Console.WriteLine("---------------------------------------");
 
-            // Beräknar pengarna kunden gav dig - vad det kostar
-            FinalPrice = MoneyFromCustomer - TotalSum;
+            //  Öresavrundning, Finalprice är lika med Totalsum avrundat, Totalsum är vad sakerna kostar. Rounding off är priset efter avrundingen Rounding off blir Kostnaden av sakerna - pengarna 
+            // som man fick av kunden.
+
+            FinalPrice = (uint)Math.Round(TotalSum);
+            RoundingOff = TotalSum - MoneyFromCustomer;
 
             //Presenterar resultatet.
-            Console.WriteLine("Vad som ska betalas är {0:f2}", FinalPrice);
+            Console.WriteLine("Totalt");
+            Console.WriteLine("Öresavrundning: {0} ", RoundingOff); // {0:f2} innebär att öresavrundningen skrivs med 2 decimaler.
+            Console.WriteLine("Att Betala");
+            Console.WriteLine("Kontant");
+            Console.WriteLine("Totalt");
+            Console.WriteLine("---------------------------------------");
+
         }
     }
 }
